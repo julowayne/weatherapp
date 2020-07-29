de8e815e538457d4782c6eeb7771fa00736ca842
Jules TD <thomas.desessarts-jules@hotmail.com>
2020-07-30 01:28:49 +0200

    fixed apikey using environement variable

--- src/components/Today.vue
+++ src/components/Today.vue
@@ -9,17 +9,28 @@
 </template>
 
 <script>
+console.log(process.env.VUE_APP_OPEN_WEATHER_API);
+require("dotenv").config();
+console.log(process.env);
+var dotenv = require("dotenv");
+var dotenvExpand = require("dotenv-expand");
+
+var myEnv = dotenv.config();
+dotenvExpand(myEnv);
+
+// const apiurl = `process.env.VUE_APP_OPEN_WEATHER_API`;
 import moment from "moment";
 export default {
   name: "Today",
   data: () => ({
     name: "",
     sys: { country: "" },
-    temperature: 27
+    temperature: 0
   }),
   created() {
     fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=Houilles&appid=374f5dd6b4f601b323b685f32da6028c&units=metric"
+      // "http://api.openweathermap.org/data/2.5/weather?q=Houilles&appid=374f5dd6b4f601b323b685f32da6028c&units=metric"
+      `http://api.openweathermap.org/data/2.5/weather?q=Houilles&appid=${process.env.VUE_APP_OPEN_WEATHER_API}&units=metric`
     )
       .then(response => response.json())
       .then(data => {
