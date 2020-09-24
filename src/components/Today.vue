6a981a37123f9526b42b852fe773a62f385c4ec6
Jules TD <thomas.desessarts-jules@hotmail.com>
2020-09-24 22:36:07 +0200

    fixed API query with geolocation coordinates

--- src/components/Today.vue
+++ src/components/Today.vue
@@ -9,36 +9,58 @@
 </template>
 
 <script>
-console.log(process.env.VUE_APP_OPEN_WEATHER_API);
-require("dotenv").config();
-console.log(process.env);
-var dotenv = require("dotenv");
-var dotenvExpand = require("dotenv-expand");
-
-var myEnv = dotenv.config();
-dotenvExpand(myEnv);
-
-// const apiurl = `process.env.VUE_APP_OPEN_WEATHER_API`;
 import moment from "moment";
 export default {
   name: "Today",
   data: () => ({
     name: "",
     sys: { country: "" },
-    temperature: 0
+    temperature: 0,
+    latitude: "",
+    longitude: ""
   }),
+  methods: {
+    getPosition() {
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+
+      var success = pos => {
+        var crd = pos.coords;
+        /*    console.log("Votre position actuelle est :");
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude : ${crd.longitude}`);
+        console.log(`La précision est de ${crd.accuracy} mètres.`);
+        console.log(this); */
+        this.latitude = crd.latitude;
+        this.longitude = crd.longitude;
+        this.fetchData();
+      };
+      function error(err) {
+        console.warn(`ERROR (${err.code}): ${err.message}`);
+      }
+
+      navigator.geolocation.getCurrentPosition(success, error, options);
+    },
+    fetchData() {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${this.latitude}&lon=${this.longitude}&appid=${process.env.VUE_APP_OPEN_WEATHER_API}&units=metric`
+      )
+        .then(response => response.json())
+        .then(data => {
+          this.temperature = data.main.temp_max;
+          this.name = data.name;
+          this.sys = data.sys.country;
+          this.latitude = data.latitude;
+          this.longitude = data.longitude;
+          console.log(data);
+        });
+    }
+  },
   created() {
-    fetch(
-      // "http://api.openweathermap.org/data/2.5/weather?q=Houilles&appid=374f5dd6b4f601b323b685f32da6028c&units=metric"
-      `https://api.openweathermap.org/data/2.5/weather?q=Houilles&appid=${process.env.VUE_APP_OPEN_WEATHER_API}&units=metric`
-    )
-      .then(response => response.json())
-      .then(data => {
-        this.temperature = data.main.temp_max;
-        this.name = data.name;
-        this.sys = data.sys.country;
-        console.log(data);
-      });
+    this.getPosition();
   },
   computed: {
     today() {
